6e4b53b2b2c9d0328c8d6224b8f08e8296053879
Andrew Shepherd <shepherd_a@hotmail.com>
2014-12-14 09:44:41 +1100

    Now there's a complete working example

---
+++ WpfMasterChild/ViewModels/Thneed.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Color = System.Windows.Media.Color;
+
+namespace WpfMasterChild.ViewModels
+{
+    public class Thneed : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    var dg = this.PropertyChanged;
+                    if (dg != null)
+                    {
+                        dg(this, new PropertyChangedEventArgs("Name"));
+                    }
+                }
+            }
+        }
+
+        private Color _color;
+        public Color ThneedColor
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (_color != value)
+                {
+                    _color = value;
+                    var dg = this.PropertyChanged;
+                    if (dg != null)
+                    {
+                        dg(this, new PropertyChangedEventArgs("ThneedColor"));
+                    }
+                }
+                }
+            }
+        }
+
+
+    }
+
---
+++ WpfMasterChild/ViewModels/ThneedCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace WpfMasterChild.ViewModels
+{
+    public class ThneedCollection
+    {
+        public ThneedCollection()
+        {
+            this.AllThneeds = new ObservableCollection<Thneed>()
+            {
+                new Thneed { Name = "Red Thneed", ThneedColor = Colors.Red },
+                new Thneed { Name = "Green Thneed", ThneedColor = Colors.Green },
+                new Thneed { Name = "Blue Thneed", ThneedColor = Colors.Blue }
+            };
+        }
+
+        public ObservableCollection<Thneed> AllThneeds
+        {
+            get;
+            set;
+        }
+    }
+}
